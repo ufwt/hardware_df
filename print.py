50ee82bb7272a76aa091a35032e1de5893fb3174
Pengfei <wpengfeinudt@google.com>
2017-04-06 18:26:18 +0800

    Rewrite print() to print results by num in their name.

--- print.py
+++ print.py
@@ -1,18 +1,38 @@
 # print the final result in a way easy for manual review
 import json
 
+# get the number from the name
+def get_num(name):
+	p1 = name.find('switched-')+9
+	p2 = name.rfind('---')
+
+	#print 'num:',name[p1:p2]
+	return name[p1:p2]
+
 def print_pretty():
 	record_file = open("record_converted.txt","r")
 	record_str = record_file.readline()
 	record_dict = json.loads(record_str)
 	record_file.close()# load and close
 
-	count = 1
+	
+	list_length = 1500
+	l=[0]*list_length #initialize a list with zeros, length 1500
+
 
 	for name in record_dict:
-		for fetches in record_dict[name]['fetch_list']:
-			print '[', count, '] file: ', name,fetches	
-			count = count + 1
+		num = int(get_num(name))
+		l[num] = record_dict[name] # sort the records by the num in their filename
 
+	count = 1
+	i = 0
+	while i < list_length:
+		if l[i] == 0:
+			pass
+		else:
+			for fetches in l[i]['fetch_list']:
+				print '[', count, '] file: ', l[i]['origin_file'],fetches	
+				count = count + 1
+		i = i + 1
 
 print_pretty()
